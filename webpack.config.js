8de06c8474a4f8e272b519c70a0ca2cc3e8497f2
Dian Hao Yap <dianhao.yap@shopee.com>
2021-05-12 11:53:33 +0800

    add png, jpg and svg images

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
                     "css-loader",
                 ],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     resolve: {
