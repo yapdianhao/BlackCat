464102390dbb408ba568f443d73f1027cc46f716
Dian Hao Yap <dianhao.yap@shopee.com>
2021-05-14 15:35:45 +0800

    add form for user login

---
+++ custom.d.ts
@@ -0,0 +1,9 @@
+declare module "*.svg" {
+    // import React = require('react');
+    // export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    // export const src: string;
+    
+    import { ReactElement, SVGProps } from "react";
+    const content: (props: SVGProps<SVGElement>) => ReactElement;
+    export default content;
+}
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { setConstantValue } from 'typescript';
+
 import "../styles/LoginScreen.scss";
 
+
 const LoginScreen = () => {
+    const [userName, setUserName] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+
+    const handleSubmit= (event: any) => {
+        console.log("submitted!");
+    }
+
     return (
         <div>
-
+            <div className="sub-title">
+                FIND THE MOST LOVED ACTIVITIES
+            </div>
+            <div className="main-title">
+                BLACK CAT
+            </div>
+            <div className="black-cat-logo">
+                <img src={require("../images/logo-cat.svg")}  />
+            </div>
+            <div className="user-login-form">
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Name:
+                        <input type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     )
 }
