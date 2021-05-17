b1b806d1100fb20b8896759e8731a856ee4152c5
Dian Hao Yap <dianhao.yap@shopee.com>
2021-05-17 17:03:10 +0800

    create divider

---
+++ components/Divider.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import "../styles/Divider.scss";
+
+const Divider = () => (
+    <hr className="red-horizontal-divider"/>
+);
+
+export default Divider;
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Counter from "../components/Counter";
 import About from "../components/About";
-import Home from "../components/Home";
+import Home from "../screens/HomeScreen";
 import LoginScreen from "../screens/LoginScreen";
 
 const App = () => {
