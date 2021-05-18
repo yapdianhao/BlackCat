d04de42e57427ba9e7ee4a4e0ffab7f84b27fb7b
Dian Hao Yap <dianhao.yap@shopee.com>
2021-05-18 23:20:42 +0800

    worked on eventsCard

---
+++ components/CheckIcon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CheckIcon = () => {
+  return (
+    <svg
+      id="Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+    >
+      <title>check</title>
+      <path d="M31.34,10.06L12.68,29.22a1,1,0,0,1-1.43,0L0.66,19.14a1,1,0,0,1,0-1.4L5,13.55a1,1,0,0,1,1.43,0L11.22,18,24.88,3.77a1,1,0,0,1,1.44,0l5,4.89A1,1,0,0,1,31.34,10.06Z" />
+    </svg>
+  );
+};
+
+export default CheckIcon;
--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -3,17 +3,32 @@ import React from "react";
 
 import "../styles/EventCard.scss";
 const profilePic = require("../images/Street-Dance-01.jpg");
+import ClockIcon from "../components/ClockIcon";
 
 const EventCard = () => {
   return (
     <div className="card-outline">
       <div className="card-header">
         <img src={String(profilePic)} className="profile-pic" />
+        <div className="user-name">Username</div>
         <div className="channel-name">Channel name</div>
       </div>
       <div className="activity-title">
         This is a very long title, may be more than one line.
       </div>
+      <div className="activity-time-area">
+        <ClockIcon />
+        <div className="activity-time">
+          14 May 2016 22:30 - 14 May 2016 23:00
+        </div>
+      </div>
+      <div className="activity-desc">
+        Morbi a metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere
+        eu, velit. Nullam sapien sem, ornare ac, nonummy non, lobortis a, enim.
+        Nunc tincidunt ante vitae massa. Duis ante orci, molestie vitae,
+        vehicula venenatis, tincidunt ac, pede. Nulla accumsan, elit sit
+      </div>
+      <div className="activity-stats-area"></div>
     </div>
   );
 };
