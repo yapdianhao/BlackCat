9a23eca4e832728368685c1c882fee55eb2d0de3
Dian Hao Yap <dianhao.yap@shopee.com>
2021-06-04 16:36:52 +0800

    remove unused reducer

--- src/utils/store/store.ts
+++ src/utils/store/store.ts
@@ -1,8 +1,4 @@
 import { createStore } from "redux";
 import allReducers from "../reducers/allReducers";
 
-export const store = createStore(
-  allReducers,
-  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = createStore(allReducers);
