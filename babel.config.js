b1cb81db25d936c56dafe5bd915fdcdb6582ff97
Dian Hao Yap <dianhao.yap@shopee.com>
2021-06-04 11:11:58 +0800

    tick readme

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-typescript"],
+  presets: [
+    "@babel/preset-env",
+    {
+      targets: {
+        edge: "17",
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+      },
+      useBuiltIns: "usage",
+      corejs: "3.6.4",
+    },
+    "@babel/preset-typescript",
+  ],
   plugins: ["@babel/plugin-syntax-dynamic-import"],
 };
